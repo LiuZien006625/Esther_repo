b7ecf5f1683bd7006b7f8548bf1dec4a4a49f793
LiuZien006625 <estherliu@harmonyschool.org>
2025-10-27 14:23:56 -0400

    Refactor number guessing game output for improved user experience

--- testing.sh
+++ testing.sh
@@ -10,6 +10,7 @@ echo -e "\nMy name is Ester\n"
 # Loop while 'count' is less than or equal to 3
 while [ $count -le 3 ]
 do
+  echo ""
   echo "I am thinking of a number between 1 and 100."
   echo "Can you guess what it is?"
   read -p "Enter a number between 1 and 100: " user_num
@@ -18,11 +19,14 @@ do
   # Increment the counter
   count=$((count + 1))
   if (( user_num == magic_num )); then
-      echo "Congratulation, you're right!"
-      count=100
-  else 
-      echo "Try again!"
+      tput setaf 2; echo "Congratulation, you're right!" ; tput sgr0
+      break
+  elif [ $count -le 3 ]; then
+      echo ""
+      tput setaf 3; echo "Try again!" ; tput sgr0
+  else
+      tput setaf 1; echo "You have failed!!!" ; tput sgr0
   fi
 done
 
-echo "Loop finished."
+tput setaf 5; echo "Game over!" ; tput sgr0
